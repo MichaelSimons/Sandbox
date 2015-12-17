cd4ec844214322f681e8aa4a7e9b4b5fba817a7e
Michael Simons <hitnmis@outlook.com>
2015-12-17 11:52:22 -0600

    Add Sample

---
+++ Samples/src/Samples/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Samples
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Tuple<string, bool>[] testInput = new Tuple<string, bool>[]
+                {
+                    Tuple.Create("", true),
+                    Tuple.Create(" ", true),
+                    Tuple.Create("a", true),
+                    Tuple.Create("aa", true),
+                    Tuple.Create("ab", false),
+                    Tuple.Create("aba", true),
+                    Tuple.Create("ab a", false),
+                    Tuple.Create("rotator", true),
+                    Tuple.Create("Rotator", false),
+                    Tuple.Create("A nut for a jar of tuna.", false),
+                };
+
+            Task[] tasks = new Task[]
+                {
+                    Validator.ValidateAsync(nameof(IsPalindromeOption1), Program.IsPalindromeOption1, testInput),
+                    Validator.ValidateAsync(nameof(IsPalindromeOption2), Program.IsPalindromeOption2, testInput),
+                };
+
+            Task.WaitAll(tasks);
+
+            Console.ReadLine();
+        }
+
+        private static bool IsPalindromeOption1(string input)
+        {
+            if (input == null)
+            {
+                throw new ArgumentNullException(nameof(input));
+            }
+
+            string revInput = "";
+            foreach (char c in input)
+            {
+                revInput = revInput.Insert(0, c.ToString());
+            }
+
+            return string.Equals(input, revInput, StringComparison.Ordinal);
+        }
+
+        private static bool IsPalindromeOption2(string input)
+        {
+            if (input == null)
+            {
+                throw new ArgumentNullException(nameof(input));
+            }
+
+            bool result = true;
+            for (int i = 0; i < (input.Length / 2); i++)
+            {
+                if (input[i] != input[input.Length - 1 - i])
+                {
+                    result = false;
+                    break;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Samples/src/Samples/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Samples")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Samples")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("fb9eea34-6d16-4dcf-9035-e35bffbf661b")]
