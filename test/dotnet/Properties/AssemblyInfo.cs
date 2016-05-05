0f3e7e6fd6f867ffc5ce7d0f076d425a16da7dec
msimons <msimons@DDMSIMONS132>
2016-05-05 12:02:17 -0700

    Misc repo cleanup and adding tests.

--- test/dotnet/Properties/AssemblyInfo.cs
+++ test/dotnet/Properties/AssemblyInfo.cs
@@ -5,14 +5,10 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Samples")]
-[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Samples")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyProduct("dotnet")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
@@ -20,4 +16,4 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("fb9eea34-6d16-4dcf-9035-e35bffbf661b")]
+[assembly: Guid("73eb0260-e241-4767-8170-7a608bf172b4")]
