9bbe8a582bc9b5d5bc6522efef38767f80c2a585
MichaelSimons <msimons@microsoft.com>
2017-03-14 17:29:35 -0500

    Add MemoryUser app

---
+++ MemoryUser/MemoryUser.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+
+namespace MemoryUser
+{
+    public static class MemoryUser
+    {
+        public static void Main(string[] args)
+        {
+            long targetMemoryUsage = long.Parse(args[0]);
+            long currentMemoryUsage;
+            List<string[]> arrays = new List<string[]>();
+
+            while(true)
+            {
+                Process currentProcess = Process.GetCurrentProcess();
+                currentMemoryUsage = currentProcess.WorkingSet64;
+                Console.WriteLine($"Current Memory Usage:  {currentMemoryUsage}");
+
+                if(currentMemoryUsage >= targetMemoryUsage)
+                {
+                    break;
+                }
+
+                Console.WriteLine("Consuming Memory...");
+                string[] strArray = new string[10000];
+                for (int i = 0; i < strArray.Length; i++)
+                {
+                    strArray[i] = $"{i}{DateTime.Now}";
+                }
+                arrays.Add(strArray);
+            };
+
+            Console.WriteLine("Press 'Enter' to exit");
+            Console.ReadLine();
+        }
+    }
+}
