b75ef189e414462e9a7a46e30e188bb6e6f6f8c2
MichaelSimons <msimons@microsoft.com>
2017-03-16 09:15:50 -0500

    Added consume memory option

--- MemoryUser/MemoryUser.cs
+++ MemoryUser/MemoryUser.cs
@@ -11,15 +11,16 @@ namespace MemoryUser
         {
             long targetMemoryUsage = long.Parse(args[0]);
             long memoryUsageGrowthFactor = long.Parse(args[1]);
+            bool consumeMemory = bool.Parse(args[3]);
             List<string[]> arrays = new List<string[]>();
 
-            while(true)
+            while (true)
             {
                 Process currentProcess = Process.GetCurrentProcess();
                 long currentMemoryUsage = currentProcess.WorkingSet64;
                 Console.WriteLine($"Current Memory Usage:  {currentMemoryUsage}");
 
-                if(currentMemoryUsage >= targetMemoryUsage)
+                if (currentMemoryUsage >= targetMemoryUsage)
                 {
                     break;
                 }
@@ -30,7 +31,10 @@ namespace MemoryUser
                 {
                     strArray[i] = $"{i}{DateTime.Now}";
                 }
-                arrays.Add(strArray);
+                if (consumeMemory)
+                {
+                    arrays.Add(strArray);
+                }
             };
 
             Thread.Sleep(60000);
