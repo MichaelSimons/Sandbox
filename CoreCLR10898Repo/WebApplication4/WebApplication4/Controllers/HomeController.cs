9013ca2dbe30724d99c84a562ee40a16d50b3547
MichaelSimons <msimons@microsoft.com>
2017-04-13 14:08:05 -0500

    Added repro scenario for https://github.com/dotnet/coreclr/issues/10898

---
+++ CoreCLR10898Repo/WebApplication4/WebApplication4/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+namespace WebApplication4.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            byte[] bytes = { 195, 184 };
+            string encodedString = Encoding.UTF8.GetString(bytes);
+            ViewData["Message"] = $"Your application description page. {encodedString}";
+
+            //ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
