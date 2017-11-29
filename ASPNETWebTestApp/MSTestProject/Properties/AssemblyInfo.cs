e94d0e2c1427fdcae9a6ed2ef7c0eff74cba6ded
MichaelSimons <msimons@microsoft.com>
2017-11-29 13:52:23 -0600

    Tweaks to test apps

---
+++ ASPNETWebTestApp/MSTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MSTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MSTestProject")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2b2a6f76-2f8c-459c-acc9-49010e83cae4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ASPNETWebTestApp/MSTestProject/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SharedLibrary;
+
+namespace MSTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string msg = "I'm a Cool Robot";
+            Assert.IsTrue(Robot.GetBot(msg).Contains(msg));
+        }
+    }
+}
