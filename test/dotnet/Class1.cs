2f0b72de8753f5168da9b3d993ab87a831c8ac67
Michael Simons <msimons@microsoft.com>
2018-04-09 16:12:57 -0500

    Update Class1.cs

--- test/dotnet/Class1.cs
+++ test/dotnet/Class1.cs
@@ -9,6 +9,11 @@ namespace dotnet
     // To enable this option, right-click on the project and select the Properties menu item. In the Build tab select "Produce outputs on build".
     public class Class1
     {
+        private static readonly string s_varShaPattern =
+            $"[ \\$](?<{EnvNameGroupName}>(dotnet_|aspnetcore_)sha512)( )*=( )*'(?<{ValueGroupName}>[^'\\s]*)'";
+
+        private static readonly Regex s_downloadUrlRegex = new Regex($"({s_envDownloadUrlPattern})|{s_inlineUrlPattern}");
+        
         public Class1()
         {
         }
